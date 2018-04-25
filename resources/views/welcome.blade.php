59bd2e65954ec617e95a2fa80ff4b96dc725be8e
BelayEl <belaynehmulat@gmail.com>
2018-04-25 14:41:57 +0200

    cores of our app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
   <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron">
       <div class="container">
-        <h1 class="display-3">Hello, world!</h1>
-        <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+        <h1 class="display-3"><b>Amram Online Acadamy</b></h1>
+        <h2><b>Associate with students of your batch and descover your future world</b></h2>
         <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
       </div>
     </div
@@ -16,18 +16,18 @@
      <div class="row">
        <div class="col-md-4">
          <h2>Heading</h2>
-         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+         <p><b>Ask questions and get excellent answers from Seniors, Teachers and Acadamic pros</b></p>
+         <p><a class="btn btn-secondary" href="#" role="button">More ... &raquo;</a></p>
        </div>
        <div class="col-md-4">
          <h2>Heading</h2>
-         <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+         <p><b>Prepare for National exams with international mind set, confidence and competence</b> </p>
+         <p><a class="btn btn-secondary" href="#" role="button">More ... &raquo;</a></p>
       </div>
        <div class="col-md-4">
          <h2>Heading</h2>
-         <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-         <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
+         <p><b>Get all the resources you need to create your future</b></p>
+         <p><a class="btn btn-secondary" href="#" role="button">More ... &raquo;</a></p>
        </div>
      </div>
 @endsection
